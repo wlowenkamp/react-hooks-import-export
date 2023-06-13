591f1f2836df699db2ea4b628f35c95524eb358b
Will Lowenkamp <lowenkamp97@gmail.com>
2023-06-13 14:48:05 -0400

    complete

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,11 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import react from "react";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
 }
+
+export default ColoradoStateParks
